9a2d9ac101669456ebf5ee3fe75022a52c6ab238
J Ho Xray <jho.xray@gmail.com>
2013-03-18 21:33:51 +0100

    initial commit

---
+++ package.js
@@ -0,0 +1,23 @@
+try {
+    Package.describe({
+        summary: "Testing framework for Observatory Suite"
+    });
+
+    Package.on_use(function (api) {
+        api.use(['coffeescript', 'bootstrap', 'underscore','templating','less'], 'client');
+
+        api.add_files('client/lib/chai.js','client');
+        api.add_files([
+            'mtest.coffee',
+            'sampleTests.coffee',
+            'client/simpleTests.html',
+            'client/mtestsTemplates.coffee'
+        ], 'client');
+
+        api.add_files('mtest.coffee','server');
+        api.add_files('sampleTests.coffee','server');
+    });
+}
+catch (err) {
+    console.log("Error while trying to load a package: " + err.message);
+};
